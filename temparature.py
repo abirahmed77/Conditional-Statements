9ed63cca24924075a77d8f98b7cd9610088098b0
Abir <abirgpx@gmail.com>
2025-01-17 12:15:40 +0600

    hi

---
+++ temparature.py
@@ -0,0 +1,7 @@
+temparature = float(input("Enter todays temparature: "))
+
+if temparature < 22:
+    print("You should wear winter clothes")
+    
+else:
+    print("You should wear light clothes")
